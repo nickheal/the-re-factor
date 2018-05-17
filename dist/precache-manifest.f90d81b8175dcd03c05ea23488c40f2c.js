d2b151e7666333a7f5ca1880792f53650a1f16cf
nickheal <nick3_4@hotmail.com>
2018-05-17 18:17:09 +0100

    stuff...

---
+++ dist/precache-manifest.f90d81b8175dcd03c05ea23488c40f2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "468b8e29019b5a65dc71",
+    "url": "/js/vendor.468b8e29.js"
+  },
+  {
+    "revision": "ce28c628eea246b643c9",
+    "url": "/js/manifest.ce28c628.js"
+  },
+  {
+    "revision": "50f78724fafb4c68fbf2",
+    "url": "/js/app.50f78724.js"
+  },
+  {
+    "revision": "68d42f5be2079e277379db72dd51aa80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50f78724fafb4c68fbf2",
+    "url": "/css/app.058c8ddb.css"
+  }
+];
