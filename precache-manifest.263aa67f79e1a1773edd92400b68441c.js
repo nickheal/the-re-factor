d1162da1ec7f45242cb63910800afa9580d4faca
nickheal <nick3_4@hotmail.com>
2018-05-18 21:39:04 +0100

    ef03a4f

---
+++ precache-manifest.263aa67f79e1a1773edd92400b68441c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4985c052852f667e9646",
+    "url": "https://nickheal.github.io/the-re-factor/js/vendor.4985c052.js"
+  },
+  {
+    "revision": "1ce8f2b0cd6ac4589e44",
+    "url": "https://nickheal.github.io/the-re-factor/js/manifest.1ce8f2b0.js"
+  },
+  {
+    "revision": "857c5a80114b5766352a",
+    "url": "https://nickheal.github.io/the-re-factor/js/app.857c5a80.js"
+  },
+  {
+    "revision": "3c793433783307f44c75c2afd3cc9b18",
+    "url": "https://nickheal.github.io/the-re-factor/index.html"
+  },
+  {
+    "revision": "857c5a80114b5766352a",
+    "url": "https://nickheal.github.io/the-re-factor/css/app.1ae91ab2.css"
+  }
+];
